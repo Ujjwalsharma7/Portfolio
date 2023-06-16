3522e1dd29d647bc49d065a0e41a3bbdc8db76be
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-17 01:27:50 +0530

    added btn class in cta

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,8 +5,8 @@ function CTA() {
   return (
   
       <div className="cta">
-        <a href={Resume} download>Download CV</a>
-        <a href="#contact">Let's Connect</a>
+        <a href={Resume} download className='btn'>Download CV</a>
+        <a href="#contact" className='btn btn-primary'>Let's Connect</a>
       </div>
    
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <div className="container header__container">
       <h5>Hello I'm</h5>
       <h1>Ujjwal Sharma</h1>
-      <h5 className='text-light'>Fullstack Developer and AI/ML enthusiast </h5>
+      <h5 className='text-light'>Fullstack Developer and AI/ML Enthusiast </h5>
       <CTA />
     </div>
    </header>
