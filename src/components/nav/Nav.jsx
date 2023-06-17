45be15b3f0f231540251a5c4bb5ae477fc4729ef
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-17 16:39:55 +0530

    added icons for navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './nav.css'
+import {AiFillHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsFillBookFill} from 'react-icons/bs'
+import {FaToolbox} from 'react-icons/fa'
+import {AiFillContacts} from 'react-icons/ai'
 function Nav() {
   return (
     <div>
-      Nav
+     <a href=""><AiFillHome/></a>
+     <a href=""><AiOutlineUser/></a>
+     <a href=""><BsFillBookFill/></a>
+     <a href=""><FaToolbox/></a>
+     <a href=""><AiFillContacts/></a>
     </div>
   )
 }
