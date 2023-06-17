ac3ffc38cb87b3ca95e252692a1cf40bc8448c46
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-18 00:06:26 +0530

    added about icons

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import me3 from '../../assets/me3.JPG'
+import {FaAward} from 'react-icons/fa'
+import {AiFillBook} from 'react-icons/ai'
+import {GrCompliance} from 'react-icons/gr'
 
 function About() {
   return (
@@ -11,14 +14,28 @@ function About() {
     <div className='container about__container'>
       <div className='about__me'>
         <div className="about__me-image">
-          <img src={me3} alt="About-image" />
+          <img src={me3}  alt=''/>
         </div>
       </div>  
     </div>
 
     <div className="about__contnet">
       <div className="about__cards">
-        
+        <article className='about__card'>
+        < AiFillBook className='about-icon'/>
+          <h5>NIT Hamirpur</h5>
+          <small>3rd Year Student </small>
+        </article>
+        <article className='about__card'>
+        <FaAward className='about-icon'/>
+          <h5>Experience</h5>
+          <small>1+ Year of Web Development  </small>
+        </article>
+        <article className='about__card'>
+        <GrCompliance className='about-icon'/>
+          <h5>Projects</h5>
+          <small> Completed 20+ projects  </small>
+        </article>
       </div>
     </div>
      </section>
