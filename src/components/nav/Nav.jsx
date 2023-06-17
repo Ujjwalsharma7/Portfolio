b663811cd2398e90cd1de1c0ab5063a8046b1303
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-17 21:42:17 +0530

    added ids to diff section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,14 +5,15 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BsFillBookFill} from 'react-icons/bs'
 import {FaToolbox} from 'react-icons/fa'
 import {AiFillContacts} from 'react-icons/ai'
+
 function Nav() {
   return (
     <div>
-     <a href=""><AiFillHome/></a>
-     <a href=""><AiOutlineUser/></a>
-     <a href=""><BsFillBookFill/></a>
-     <a href=""><FaToolbox/></a>
-     <a href=""><AiFillContacts/></a>
+     <a href="#"><AiFillHome/></a>
+     <a href="#about"><AiOutlineUser/></a>
+     <a href="#education"><BsFillBookFill/></a>
+     <a href="#experience"><FaToolbox/></a>
+     <a href="#contact"><AiFillContacts/></a>
     </div>
   )
 }
