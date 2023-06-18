1632a7b9fd2c455e65fb376f37d19b9c08e81538
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-18 15:28:15 +0530

    added css to content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,13 +12,13 @@ function About() {
       <h2>About Me</h2>
 
       <div className='container about__container'>
-        <div className="about__me">
+        <div className="about-me">
           <div className="about__me-image">
             <img src={me3} alt='' />
           </div>
         </div>
 
-        <div className="about__contnet">
+        <div className="about__content">
           <div className="about__cards">
             <article className='about__card'>
               < AiFillBook className='about-icon' />
