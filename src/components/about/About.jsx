947ab3eb58b7b88f02586f61fad0428176ed4303
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-18 14:18:32 +0530

    added css for about image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import './about.css'
 import me3 from '../../assets/me3.JPG'
 import {FaAward} from 'react-icons/fa'
 import {AiFillBook} from 'react-icons/ai'
-import {GrCompliance} from 'react-icons/gr'
+import {AiOutlineFileDone} from 'react-icons/ai'
 
 function About() {
   return (
@@ -17,7 +17,6 @@ function About() {
           <img src={me3}  alt=''/>
         </div>
       </div>  
-    </div>
 
     <div className="about__contnet">
       <div className="about__cards">
@@ -32,12 +31,13 @@ function About() {
           <small>1+ Year of Web Development  </small>
         </article>
         <article className='about__card'>
-        <GrCompliance className='about-icon'/>
+        <AiOutlineFileDone className='about-icon'/>
           <h5>Projects</h5>
-          <small> Completed 20+ projects  </small>
+          <small> Completed 20+ projects </small>
         </article>
       </div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odio molestiae harum quos, modi quod corporis corrupti exercitationem illum rerum repudiandae necessitatibus sequi odit repellat quidem sint quis eius impedit fuga, qui ipsam. Eum, optio voluptates veritatis a sapiente possimus ipsum maxime, vitae error iste soluta cumque ullam? Porro, qui!</p>
+    </div>
     </div>
      </section>
   )
