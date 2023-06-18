1000d4288c0811129c9d31a156fd38bb1a7cbe4d
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-18 15:01:45 +0530

    added css to about cards

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,46 +1,45 @@
 import React from 'react'
 import './about.css'
 import me3 from '../../assets/me3.JPG'
-import {FaAward} from 'react-icons/fa'
-import {AiFillBook} from 'react-icons/ai'
-import {AiOutlineFileDone} from 'react-icons/ai'
+import { FaAward } from 'react-icons/fa'
+import { AiFillBook } from 'react-icons/ai'
+import { AiOutlineFileDone } from 'react-icons/ai'
 
 function About() {
   return (
-  <section id='about'>
-    <h5>Get To Know</h5>
-    <h2>About Me</h2>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-    <div className='container about__container'>
-
-      <div className="about__me">
-        <div className="about__me-image">
-          <img src={me3}  alt=''/>
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={me3} alt='' />
+          </div>
         </div>
-      </div>  
 
-    <div className="about__contnet">
-      <div className="about__cards">
-        <article className='about__card'>
-        < AiFillBook className='about-icon'/>
-          <h5>NIT Hamirpur</h5>
-          <small>3rd Year Student </small>
-        </article>
-        <article className='about__card'>
-        <FaAward className='about-icon'/>
-          <h5>Experience</h5>
-          <small>1+ Year of Web Development  </small>
-        </article>
-        <article className='about__card'>
-        <AiOutlineFileDone className='about-icon'/>
-          <h5>Projects</h5>
-          <small> Completed 20+ projects </small>
-        </article>
+        <div className="about__contnet">
+          <div className="about__cards">
+            <article className='about__card'>
+              < AiFillBook className='about-icon' />
+              <h5>NITH</h5>
+              <small>3rd Year</small>
+            </article>
+            <article className='about__card'>
+              <FaAward className='about-icon' />
+              <h5>Experience</h5>
+              <small>1+ Year </small>
+            </article>
+            <article className='about__card'>
+              <AiOutlineFileDone className='about-icon' />
+              <h5>Projects</h5>
+              <small> 20+ Completed</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odio molestiae harum quos, modi quod corporis corrupti exercitationem illum rerum repudiandae necessitatibus sequi odit repellat quidem sint quis eius impedit fuga, qui ipsam. Eum, optio voluptates veritatis a sapiente possimus ipsum maxime, vitae error iste soluta cumque ullam? Porro, qui!</p>
+        </div>
       </div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odio molestiae harum quos, modi quod corporis corrupti exercitationem illum rerum repudiandae necessitatibus sequi odit repellat quidem sint quis eius impedit fuga, qui ipsam. Eum, optio voluptates veritatis a sapiente possimus ipsum maxime, vitae error iste soluta cumque ullam? Porro, qui!</p>
-    </div>
-    </div>
-     </section>
+    </section>
   )
 }
 
