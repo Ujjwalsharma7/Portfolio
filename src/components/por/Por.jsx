c3daadfebbaecacb419d93f4a3b983f137f39e46
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-20 21:44:03 +0530

    added jsx for por

--- src/components/por/Por.jsx
+++ src/components/por/Por.jsx
@@ -4,7 +4,16 @@ import './por.css'
 function Por() {
   return (
     <section id='por'>
-      Por
+      <h5>Watch My</h5>
+      <h2>Positions of Responsibilty</h2>
+
+      <div className="container por__container">
+        <article className='por'>
+        <div className="logo">
+          <img src="" alt="" />
+        </div>
+        </article>
+      </div>
     </section>
   )
 }
