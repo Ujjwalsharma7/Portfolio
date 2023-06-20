9379a1e991ec8546dbfee4acc5d93675fc1f04c0
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-20 16:23:00 +0530

    started portfolio - added demo proj imgs

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,17 @@ import './portfolio.css'
 function portfolio() {
   return (
     <section id='portfolio'>
-      portfolio
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image"></div>
+          <h3>Title</h3>
+          <a href="https://github.com" className='btn'>Github</a>
+          <a href="https://dribbble.com/shots/popular" className='btn btn-primary' target='_blank '></a>
+        </article>
+      </div>
     </section>
   )
 }
