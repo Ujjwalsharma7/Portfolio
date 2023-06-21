e555ab6850d702d5a5e963e4248dd1ae0cb81815
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-21 21:10:45 +0530

    added css to por container and logo

--- src/components/por/Por.jsx
+++ src/components/por/Por.jsx
@@ -12,10 +12,27 @@ function Por() {
         <article className='por'>
         <div className="logo">
           <img src={logo} alt="" />
-          <h3 className='org__name'>Team Vibhav</h3>
-          <small>Executive Member</small>
         </div>
-        </article>
+          <h3 className='org__name'>Team Vibhav</h3>
+          <h5>Executive Member</h5>
+          <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nisi placeat molestias. Voluptates iste eos sed explicabo est consequuntur! Beatae!</small>
+        </article>  
+        <article className='por'>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+          <h3 className='org__name'>Team Vibhav</h3>
+          <h5>Executive Member</h5>
+          <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nisi placeat molestias. Voluptates iste eos sed explicabo est consequuntur! Beatae!</small>
+        </article> 
+        <article className='por'>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+          <h3 className='org__name'>Team Vibhav</h3>
+          <h5>Executive Member</h5>
+          <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nisi placeat molestias. Voluptates iste eos sed explicabo est consequuntur! Beatae!</small>
+        </article> 
       </div>
     </section>
   )
