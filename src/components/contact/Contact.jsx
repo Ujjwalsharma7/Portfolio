a52d4004703ac7e8be45e63028e9652ac49204c7
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-22 13:24:13 +0530

    added jsx for email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,19 @@ import './contact.css'
 const Contact = () => {
   return (
    <section id='contact'>
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+
+      <div className='container contact__container'>
+        <div className="contact__options">
+          <article className="contact__option">
+            <h4> Email</h4>
+            <h5>ujjwalsharma1772003@gmail.com</h5>
+            <a href="mailto:ujjwalsharma1772003@gmail.com">Send a message</a>
+          </article>
+        </div>
+      </div>
    </section>
       
     
