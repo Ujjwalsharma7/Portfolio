f67f9c148bb0394c4915706bf3f676b55213879b
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-23 16:54:09 +0530

    added sendemail function in contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 import './contact.css'
 import {AiOutlineMail} from 'react-icons/ai'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
    <section id='contact'>
       <h5>Get In Touch</h5>
@@ -25,7 +40,7 @@ const Contact = () => {
           </article>
       
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail} >
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" id="" cols="30" rows="7" placeholder='Your Message' required></textarea>
