ef5a9125749ea26a921f4d182b5e8fce58e496cd
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-25 13:57:33 +0530

    added css to footer socials  a tag

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,10 +8,10 @@ function Footer() {
   return (
 <footer>
 
-      <a href="#home" className='footer__logo'>Ujjwalsharma7</a>
+      <a href="#header" className='footer__logo'>Ujjwalsharma7</a>
 
       <ul className='permalinks'>
-      <li><a href="#home"> Home</a></li>
+      <li><a href="#header"> Home</a></li>
       <li><a href="#about"> About</a></li>
       <li><a href="#education"> Education</a></li>
       <li><a href="#experience">Experience </a></li>
