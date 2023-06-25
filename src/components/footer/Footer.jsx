1f404a0fa60d55295b9604828237ff408cdc9a92
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-25 13:32:30 +0530

    added jsx for footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,30 @@ import './footer.css'
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+<footer>
+
+      <a href="#home" className='footer__logo'>Ujjwalsharma7</a>
+
+      <ul className='permalinks'>
+      <li><a href="#home"> Home</a></li>
+      <li><a href="#about"> About</a></li>
+      <li><a href="#education"> Education</a></li>
+      <li><a href="#experience">Experience </a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#por"> Positons of Responsibilty</a></li>
+      <li><a href="#contact">   Contact</a></li>
+      
+      </ul>
+
+      <div>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
+
+</footer>
+
+    
   )
 }
 
