ef1ae43b41cee0751f7792f124928aa7708ae2fe
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-28 18:34:38 +0530

    added type-animation to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,31 @@ import './header.css'
 import CTA from './CTA'
 import me2 from '../../assets/me2.JPG'
 import HeaderSocials from './HeaderSocials'
+import { TypeAnimation } from 'react-type-animation'
 function Header() {
   return (
    <header id='header' >
     <div className="container header__container">
       <h5>Hello I'm</h5>
       <h1>Ujjwal Sharma</h1>
-      <h5 className='text-light'>Fullstack Developer and AI/ML Enthusiast </h5>
+      {/* <h5 className='text-light'>Fullstack Developer and AI/ML Enthusiast </h5> */}
+      <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Full Stack Developer ',
+        2000, // wait 1s before replacing "Mice" with "Hamsters"
+        'AI/ML Enthusiast ',
+        2000,
+        // 'We produce food for Guinea Pigs',
+        // 1000,
+        // 'We produce food for Chinchillas',
+        // 1000
+      ]}
+      // wrapper="span"
+      speed={40}
+      style={{ fontSize: '1.5em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
       <CTA />
        <HeaderSocials />
       <div className='me'>
